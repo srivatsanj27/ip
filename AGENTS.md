a200b4a169d2514ce44ff34d6a2ad15c3f7ef1c9
srivatsan <srivatsan.jumbunathan@gmail.com>
2026-08-20 13:53:15 +0800

    Renamed Main class and added Greeting/Exit

--- AGENTS.md
+++ AGENTS.md
@@ -9,8 +9,8 @@ Unless the user says otherwise, assume that you are assisting a student working 
 # Student profile
 
 * Prior knowledge: Basic Java and OOP concepts.
-* Level of programming experience: [to be filled]
-* IDE and level of expertise: [to be filled]
+* Level of programming experience: Medium (done CS1101S + CS2030S + CS2040S in NUS)
+* IDE and level of expertise: intelliJ
 
 # Guidance for interacting with users
 
---
+++ src/main/java/Ace.java
@@ -0,0 +1,19 @@
+public class Ace {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String ACE_Logo = "   ___   _____  _____\n"
+                + "  / _ \\ /  __ \\|  ___|\n"
+                + " / /_\\ \\| /  \\| |__  \n"
+                + " |  _  || |    |  __| \n"
+                + " | | | || \\__/\\| |___ \n"
+                + " \\_| |_/ \\____/\\____/\n";
+
+        System.out.println("Hello from\n" + ACE_Logo);
+        System.out.println(line);
+        System.out.println(" Hello! I'm ACE!");
+        System.out.println(" What can I do for you?");
+        System.out.println(line);
+        System.out.println(" Bye. I hope to see you again soon!");
+        System.out.println(line);
+    }
+}
