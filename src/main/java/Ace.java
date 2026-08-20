255c427239d40c3f5bd27ea6a89c090195798f05
srivatsan <srivatsan.jumbunathan@gmail.com>
2026-08-20 23:11:18 +0800

    feat: Echo

--- src/main/java/Ace.java
+++ src/main/java/Ace.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ace {
     public static void main(String[] args) {
         String line = "____________________________________________________________";
@@ -13,7 +15,23 @@ public class Ace {
         System.out.println(" Hello! I'm ACE!");
         System.out.println(" What can I do for you?");
         System.out.println(line);
-        System.out.println(" Bye. I hope to see you again soon!");
-        System.out.println(line);
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            System.out.println(line);
+
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(" " + input);
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
